9138a6ebe308a0a2913b7998fca2469ea01fce4b
khanhvu1611 <khanh.vu1611@outlook.com>
2016-04-14 22:37:36 +0700

    init base class

---
+++ DependencyRegistrar.cs
@@ -0,0 +1,32 @@
+using Autofac;
+using Nop.Core.Configuration;
+using Nop.Core.Infrastructure;
+using Nop.Core.Infrastructure.DependencyManagement;
+
+namespace Nop.Plugin.ExternalAuth.FacebookAccountKit
+{
+    /// <summary>
+    /// Dependency registrar
+    /// </summary>
+    public class DependencyRegistrar : IDependencyRegistrar
+    {
+        /// <summary>
+        /// Register services and interfaces
+        /// </summary>
+        /// <param name="builder">Container builder</param>
+        /// <param name="typeFinder">Type finder</param>
+        /// <param name="config">Config</param>
+        public virtual void Register(ContainerBuilder builder, ITypeFinder typeFinder, NopConfig config)
+        {
+            //builder.RegisterType<FacebookProviderAuthorizer>().As<IOAuthProviderFacebookAuthorizer>().InstancePerLifetimeScope();
+        }
+
+        /// <summary>
+        /// Order of this dependency registrar implementation
+        /// </summary>
+        public int Order
+        {
+            get { return 1; }
+        }
+    }
+}
---
+++ RouteProvider.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Nop.Web.Framework.Mvc.Routes;
+
+namespace Nop.Plugin.ExternalAuth.FacebookAccountKit
+{
+    public partial class RouteProvider : IRouteProvider
+    {
+        public void RegisterRoutes(RouteCollection routes)
+        {
+            routes.MapRoute("Plugin.ExternalAuth.FacebookAccountKit.Login",
+                 "Plugins/ExternalAuthFacebookAccountKit/Login",
+                 new { controller = "ExternalAuthFacebookAccountKit", action = "Login" },
+                 new[] { "Nop.Plugin.ExternalAuth.FacebookAccountKit.Controllers" }
+            );
+
+            routes.MapRoute("Plugin.ExternalAuth.FacebookAccountKit.LoginCallback",
+                 "Plugins/ExternalAuthFacebook/LoginCallback",
+                 new { controller = "ExternalAuthFacebookAccountKit", action = "LoginCallback" },
+                 new[] { "Nop.Plugin.ExternalAuth.FacebookAccountKit.Controllers" }
+            );
+        }
+        public int Priority
+        {
+            get
+            {
+                return 0;
+            }
+        }
+    }
+}
